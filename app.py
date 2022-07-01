8e48b41f94dfd9d3825d1947a6fdd678502dd714
Yara Kyrychenko <70731529+yarakyrychenko@users.noreply.github.com>
2022-07-01 13:00:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -14,14 +14,7 @@ st.warning("""
            You can ask for your data to be deleted by emailing us with an app ID number you'll be issued after submitting the form. 
            """)
 
-container = st.container()
-st.sidebar.write("Hello")
-   
-if st.slider("Slide me", 0, 100, step=1) == 100:
-    with container:
-        expander = st.expander("Expand me",expanded=True)
-        with expander:
-            st.write("Oh noes")
+container = st.container()   
 
 st.text_input("Enter a twitter username to begin", key="name")
 
@@ -34,45 +27,44 @@ if st.session_state.last_name != st.session_state.name:
             "I confirm that",
             ('This username belongs to me.', 'This username is belongs to someone else.')) 
     
-    container = st.container()
-    with st.expander("Form", expanded=True):
+    if "submitted" not in st.session_state:
+        with container:
+            if st.session_state.username_mine == 'This username belongs to me.':
 
-        if st.session_state.username_mine == 'This username belongs to me.':
+                with st.form("my_form"):
+                    dem_words = []
+                    st.markdown("#### Please add five words that describe Democrats best")
+                    for i in range(5):
+                        dem_words.append(st.text_input("D"+str(i+1)))
+                    st.session_state.dem_words = ", ".join(dem_words).lower()
 
-            with st.form("my_form"):
-                dem_words = []
-                st.markdown("#### Please add five words that describe Democrats best")
-                for i in range(5):
-                    dem_words.append(st.text_input("D"+str(i+1)))
-                st.session_state.dem_words = ", ".join(dem_words).lower()
+                    rep_words = []
+                    st.markdown("#### Please add five words that describe Republicans best")
+                    for i in range(5):
+                        rep_words.append(st.text_input("R"+str(i+1),key = "R"+str(i+1)))
+                    st.session_state.rep_words = ", ".join(rep_words).lower()
 
-                rep_words = []
-                st.markdown("#### Please add five words that describe Republicans best")
-                for i in range(5):
-                    rep_words.append(st.text_input("R"+str(i+1),key = "R"+str(i+1)))
-                st.session_state.rep_words = ", ".join(rep_words).lower()
-
-                st.markdown("#### Feeling Thermomether")
-                st.slider("How warm do you feel about Democrats (0 = coldest rating; 100 = warmest rating)?", 
-                    min_value=0, max_value=100, value=50, step=1,key="dem_temp")          
-                st.slider("How warm do you feel about Republicans (0 = coldest rating; 100 = warmest rating)?", 
-                    min_value=0, max_value=100, value=50, step=1,key="rep_temp") 
-                st.session_state.party = st.radio(
-                    "How do you identify?",
-                ('Independant','Republican', 'Democrat')) 
+                    st.markdown("#### Feeling Thermomether")
+                    st.slider("How warm do you feel about Democrats (0 = coldest rating; 100 = warmest rating)?", 
+                        min_value=0, max_value=100, value=50, step=1,key="dem_temp")          
+                    st.slider("How warm do you feel about Republicans (0 = coldest rating; 100 = warmest rating)?", 
+                        min_value=0, max_value=100, value=50, step=1,key="rep_temp") 
+                    st.session_state.party = st.radio(
+                     "How do you identify?",
+                    ('Independant','Republican', 'Democrat')) 
     
-                st.session_state.submitted = st.form_submit_button("Submit")
-    
-        if st.session_state.submitted:
-            with container:
-                if (st.session_state.rep_words[-2:] != ", "):
-                    from helper import connect_to_gsheets, insert_user_data
-                    from datetime import datetime
-                    from uuid import uuid4
+                    st.session_state.submitted = st.form_submit_button("Submit")
+
+    elif st.session_state.submitted:
+        with container:
+            if (st.session_state.rep_words[-2:] != ", "):
+                from helper import connect_to_gsheets, insert_user_data
+                from datetime import datetime
+                from uuid import uuid4
 
                 
-                    st.session_state.id = datetime.now().strftime('%Y%m-%d%H-%M-') + str(uuid4())
-                    st.session_state.conn = connect(":memory:", 
+                st.session_state.id = datetime.now().strftime('%Y%m-%d%H-%M-') + str(uuid4())
+                st.session_state.conn = connect(":memory:", 
                         adapter_kwargs = {
                     "gsheetsapi": { 
                         "service_account_info":  st.secrets["gcp_service_account"] 
@@ -80,35 +72,35 @@ if st.session_state.last_name != st.session_state.name:
                                         }
                     )
 
-                    insert_user_data(conn, st.secrets["private_gsheets_url"])
+                insert_user_data(conn, st.secrets["private_gsheets_url"])
 
-                    st.markdown("### Thanks for submitting your answers!")
-                    st.markdown(f"Your app ID is {st.session_state.id}. Note it down and email us if you want your answers deleted.") 
-                    st.success("Open the sidebar and navigate to 'Linguistic Analysis' or  'Polarization' to see your results.")
-                    with st.sidebar:
-                        st.markdown(f"👉 Click 'Linguistic Analysis' to find out what language {st.session_state.name} and others use on Twitter.")
-                        st.markdown(f"👉 Click 'Polarization' to find out how {st.session_state.name} and others think and talk about the US political parties.")
+                st.markdown("### Thanks for submitting your answers!")
+                st.markdown(f"Your app ID is {st.session_state.id}. Note it down and email us if you want your answers deleted.") 
+                st.success("Open the sidebar and navigate to 'Linguistic Analysis' or  'Polarization' to see your results.")
+
+                st.markdown(f"👉 Click 'Linguistic Analysis' to find out what language {st.session_state.name} and others use on Twitter.")
+                st.markdown(f"👉 Click 'Polarization' to find out how {st.session_state.name} and others think and talk about the US political parties.")
                         
 
-                else:
-                    st.error("Please fill out every field and try again.")
+            else:
+                st.error("Please fill out every field and try again.")
 
-        if st.session_state.username_mine == 'This username is belongs to someone else.' and st.session_state.name != "":
-            st.session_state.conn = connect(":memory:", 
+    if st.session_state.username_mine == 'This username is belongs to someone else.' and st.session_state.name != "":
+        st.session_state.conn = connect(":memory:", 
                     adapter_kwargs = {
                         "gsheetsapi": { 
                         "service_account_info":  st.secrets["gcp_service_account"] 
                                     }
                                         }
                     )
-            st.warning("""You entered someone else's Twitter username. 
+        st.warning("""You entered someone else's Twitter username. 
                 Some analyses will not be available. 
                 If you change your mind at any point, return to this page to enter your Twitter username.
                 """)
-            st.success("Open the sidebar and navigate to 'Linguistic Analysis' or  'Polarization' to see your results.")
-            with st.sidebar:
-                st.markdown(f"👉 Click 'Linguistic Analysis' to find out what language {st.session_state.name} and others use on Twitter.")
-                st.markdown(f"👉 Click 'Polarization' to find out how {st.session_state.name} and others think and talk about the US political parties.")
+        st.success("Open the sidebar and navigate to 'Linguistic Analysis' or  'Polarization' to see your results.")
+
+        st.markdown(f"👉 Click 'Linguistic Analysis' to find out what language {st.session_state.name} and others use on Twitter.")
+        st.markdown(f"👉 Click 'Polarization' to find out how {st.session_state.name} and others think and talk about the US political parties.")
 
 
 
