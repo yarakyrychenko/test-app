1df3513132586438691da807af5a974f6748e329
Yara Kyrychenko <70731529+yarakyrychenko@users.noreply.github.com>
2022-07-01 17:25:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import streamlit as st
 from shillelagh.backends.apsw.db import connect
+from helper import connect_to_gsheets, insert_user_data
+from datetime import datetime
+from uuid import uuid4
 
 st.set_page_config(
     page_title="App Home",
@@ -32,68 +35,51 @@ container = st.container()
 if st.session_state.last_name != st.session_state.name:  
     st.session_state.last_name = st.session_state.name
 
-    container = st.container()   
-
     if st.session_state.username_mine == 'This username belongs to me.':
         if st.session_state.open_form:
-            with st.expander("Form"):
+            with st.form("my_form"):
+                dem_words, rep_words = [], []
+                st.markdown("#### Please add five words that describe Democrats best")
+                for i in range(5):
+                    dem_words.append(st.text_input("D"+str(i+1)))
+                st.session_state.dem_words = ", ".join(dem_words).lower()
 
-                with st.form("my_form"):
-                    dem_words = []
-                    st.markdown("#### Please add five words that describe Democrats best")
-                    for i in range(5):
-                        dem_words.append(st.text_input("D"+str(i+1)))
-                    st.session_state.dem_words = ", ".join(dem_words).lower()
+                st.markdown("#### Please add five words that describe Republicans best")
+                for i in range(5):
+                    rep_words.append(st.text_input("R"+str(i+1),key = "R"+str(i+1)))
+                st.session_state.rep_words = ", ".join(rep_words).lower()
 
-                    rep_words = []
-                    st.markdown("#### Please add five words that describe Republicans best")
-                    for i in range(5):
-                        rep_words.append(st.text_input("R"+str(i+1),key = "R"+str(i+1)))
-                    st.session_state.rep_words = ", ".join(rep_words).lower()
-
-                    st.markdown("#### Feeling Thermomether")
-                    st.slider("How warm do you feel about Democrats (0 = coldest rating; 100 = warmest rating)?", 
-                        min_value=0, max_value=100, value=50, step=1,key="dem_temp")          
-                    st.slider("How warm do you feel about Republicans (0 = coldest rating; 100 = warmest rating)?", 
+                st.markdown("#### Feeling Thermomether")
+                st.slider("How warm do you feel about Democrats (0 = coldest rating; 100 = warmest rating)?", 
+                    min_value=0, max_value=100, value=50, step=1,key="dem_temp")          
+                st.slider("How warm do you feel about Republicans (0 = coldest rating; 100 = warmest rating)?", 
                         min_value=0, max_value=100, value=50, step=1,key="rep_temp") 
-                    st.session_state.party = st.radio(
+                st.session_state.party = st.radio(
                      "How do you identify?",
                     ('Independant','Republican', 'Democrat')) 
 
-                    st.session_state.submitted = st.form_submit_button("Submit")
+                def submit():
                     if (st.session_state.rep_words[-2:] != ", "):
                         st.session_state.open_form = False
-                    else:
-                        st.error("Please fill out every field of the form and submit again.")
-    
-            
-
-if st.session_state.submitted and not st.session_state.open_form:
-        from helper import connect_to_gsheets, insert_user_data
-        from datetime import datetime
-        from uuid import uuid4
-
-                
-        st.session_state.id = datetime.now().strftime('%Y%m-%d%H-%M-') + str(uuid4())
-        st.session_state.conn = connect(":memory:", 
-                        adapter_kwargs = {
-                    "gsheetsapi": { 
-                        "service_account_info":  st.secrets["gcp_service_account"] 
+                        st.session_state.id = datetime.now().strftime('%Y%m-%d%H-%M-') + str(uuid4())
+                        st.success("Thanks for submitting your answers!")
+                        st.markdown(f"Your app ID is {st.session_state.id}. Note it down and email us if you want your answers deleted.") 
+                        
+                        st.session_state.conn = connect(":memory:", 
+                            adapter_kwargs = {
+                            "gsheetsapi": { 
+                            "service_account_info":  st.secrets["gcp_service_account"] 
                                     }
                                         }
-                    )
-
-        #insert_user_data(conn, st.secrets["private_gsheets_url"])
-
-        st.markdown("### Thanks for submitting your answers!")
-        st.markdown(f"Your app ID is {st.session_state.id}. Note it down and email us if you want your answers deleted.") 
-        st.success("Open the sidebar and navigate to 'Linguistic Analysis' or  'Polarization' to see your results.")
-
-        st.markdown(f"👉 Click 'Linguistic Analysis' to find out what language {st.session_state.name} and others use on Twitter.")
-        st.markdown(f"👉 Click 'Polarization' to find out how {st.session_state.name} and others think and talk about the US political parties.")
-                        
-
-if st.session_state.username_mine == 'This username is belongs to someone else.' and st.session_state.name != "":
+                        )
+                        #insert_user_data(conn, st.secrets["private_gsheets_url"])
+                        st.session_state.submitted = True   
+                    else:
+                        st.error("Please fill out every field of the form and submit again.")
+                    
+                st.form_submit_button("Submit", on_click=submit)
+    
+    elif st.session_state.username_mine == 'This username is belongs to someone else.':
         st.session_state.conn = connect(":memory:", 
                     adapter_kwargs = {
                         "gsheetsapi": { 
@@ -105,10 +91,10 @@ if st.session_state.username_mine == 'This username is belongs to someone else.'
                 Some analyses will not be available. 
                 If you change your mind at any point, return to this page to enter your Twitter username.
                 """)
-        st.success("Open the sidebar and navigate to 'Linguistic Analysis' or  'Polarization' to see your results.")
 
-        st.markdown(f"👉 Click 'Linguistic Analysis' to find out what language {st.session_state.name} and others use on Twitter.")
-        st.markdown(f"👉 Click 'Polarization' to find out how {st.session_state.name} and others think and talk about the US political parties.")
+            
+        
+                        
 
 
 
